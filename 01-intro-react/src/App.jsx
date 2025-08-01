482a9394d887ff8d4225ec620c63dcb3211ecc76
Araceli Cruz <aracelicruzvazquez19@gmail.com>
2025-07-31 21:17:40 -0600

    Entregable 1

---
+++ 01-intro-react/src/App.jsx
@@ -0,0 +1,15 @@
+import Tarjeta from './components/Tarjeta'
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <div>
+        <h1>Tarjeta de presentacion</h1>
+        <Tarjeta />
+      </div>
+    </>
+  )
+}
+
+export default App
